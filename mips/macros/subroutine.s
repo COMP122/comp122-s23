7f71d07915f90c0b46028da38d93c7cae89e08cf
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-04-09 17:06:23 -0700

    incorporated clean names for macros

--- mips/macros/subroutine.s
+++ mips/macros/subroutine.s
@@ -1,42 +1,53 @@
-.macro pop_results()
+.macro save_state()
+       push($gp, $sp, $fp, $ra)
+       push_t_registers()
 .end_macro
 
-.macro pop_results(%v0)
-       nop                      # Demarshaling Input Args
+.macro restore_state()
+       pop_t_registers()
+       pop($gp, $sp, $fp, $ra)
+.end_macro
+
+
+.macro demarshal_results()
+.end_macro
+
+.macro demarshal_results(%v0)
+       nop                      # Demarshaling Return Values
        move %v0, $v0            # On MIPS, the first return value is in $v0
 .end_macro
 
-.macro pop_results(%v0, %v1)
-       nop                      # Demarshaling Input Args
+.macro demarshal_results(%v0, %v1)
+       nop                      # Demarshaling Return Values
        move %v0, $v0            # On MIPS, the first return value is in $v0
        move %v1, $v1            # On MIPS, the second return value is in $v1
 .end_macro
 
-.macro push_args()
+.macro marshal_args()
 .end_macro
-.macro push_args(%a0)
+.macro marshal_args(%a0)
         nop                     # Marshaling Input Args
         move $a0, %a0           # On MIPS, the first four args are in registers
 .end_macro
-.macro push_args(%a0,%a1)
+.macro marshal_args(%a0,%a1)
         nop                     # Marshaling Input Args
         move $a0, %a0           # On MIPS, the first four args are in registers
         move $a1, %a1
 .end_macro
-.macro push_args(%a0,%a1,%a2)
+.macro marshal_args(%a0,%a1,%a2)
         nop                     # Marshaling Input Args
         move $a0, %a0           # On MIPS, the first four args are in registers
         move $a1, %a1
   move $a2, %a2
 .end_macro
-.macro push_args(%a0,%a1,%a2,%a3)
+.macro marshal_args(%a0,%a1,%a2,%a3)
         nop                     # Marshaling Input Args
         move $a0, %a0           # On MIPS, the first four args are in registers
         move $a1, %a1
         move $a2, %a2
         move $a3, %a3
 .end_macro
-.macro push_args(%a0,%a1,%a2,%a3,%a4)
+.macro marshal_args(%a0,%a1,%a2,%a3,%a4)
         nop                     # Marshaling Input Args
         move $a0, %a0           # On MIPS, the first four args are in registers
         move $a1, %a1
